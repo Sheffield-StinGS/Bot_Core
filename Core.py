b241c5a0b618082f507307ba47586f6051f6a295
Oscar <oscarlodgelego@gmail.com>
2024-07-09 19:08:54 +0100

    Add comments

--- Core.py
+++ Core.py
@@ -1,5 +1,5 @@
 from .colours import colour_list #Imports the defined stings colours in python form
-from .imports import *
+from .imports import * #Imports all of the required libraries
 
 #Loads the General config file and sets the core config variable
 config_file = open("config.json")
@@ -15,24 +15,25 @@ time_format = '%d/%m/%y %H:%M:%S'
 #Creates the bot object with a command prefix of either tagging it or the specified value from the JSON config file
 #Also declares the intents and that the case doesn't matter when using commands
 def createBot(name):
-    global bot_config
-    bot_config = config[name]
-    DISCORD_TOKEN = bot_config["Token"]
-    global bot
-    bot = commands.Bot(command_prefix=commands.when_mentioned_or(bot_config["Command_Prefix"]), case_insensitive=True, intents=intents)
-    bot.remove_command("help")
+    global bot_config #Sets the variable to be accessable from anywhere in the code
+    bot_config = config[name] #Loads the specific block of config that is unique to the named bot
+    DISCORD_TOKEN = bot_config["Token"] #Sets the discord token variable to be the one specific to the bot
+    command_prefix = commands.when_mentioned_or(bot_config["Command_Prefix"]) #Sets the command prefix to be either when the bot is tagged or the specific value from the config file
+    global bot # Makes the bot object accesable from anywhere in the code
+    bot = commands.Bot(command_prefix=command_prefix, case_insensitive=True, intents=intents) #Creates the bot using the previously declared intents and command prefix. Also sets it to not care about case for commands
+    bot.remove_command("help") #Removes the inbuilt help command to be replaced with our custom one
     return DISCORD_TOKEN
 
 #Prints a message to the output with the time added
 def printLog(message):
-    time = datetime.now().strftime(time_format)
-    print(f'[{time}] {message}')
+    time = datetime.now().strftime(time_format) #Creates the time stamp in the specified format 
+    print(f'[{time}] {message}') #Prints the message to the log with the time stamp added
 
 #Prints a message to the output whenever a command is triggered
 def logCommand(context):
-    command = context.invoked_with
-    author = context.author
-    printLog(f'Command: \'{command}\' was triggered by \'{author}\'')
+    command = context.invoked_with #Gets the command that was used
+    author = context.author #Gets the person who used the command
+    printLog(f'Command: \'{command}\' was triggered by \'{author}\'') #Sends the message to the logging function to format and then print
 
 def main():
     @bot.event
@@ -41,17 +42,17 @@ def main():
     
     @bot.command(name="Git", help="This provides a link to the Github Repository that contains all the code that makes me work")
     async def git(ctx):
-        logCommand(ctx)
-        git_link = bot_config["Git_Repo"]
+        logCommand(ctx) #Logs that the command has been triggered
+        git_link = bot_config["Git_Repo"] #Loads the link from the config file data
         message = "Here's the Git Repo that contains all of my code. Feel free to take a look around and see how I work \n" + git_link
-        await ctx.send(message)
+        await ctx.send(message) #Sends a message that contains the git link to discord
 
     @bot.command(name="Help", help="Shows this help menu")
     async def help(ctx):
-        logCommand(ctx)
-        help_menu = discord.Embed(title=bot_config["Name"], url=bot_config["Git_Repo"], description=bot_config["Description"], colour=colour_list["stings_light_gold"])
-        help_menu.set_thumbnail(url="https://ssago.org/img/clubs/logos/23.png")
+        logCommand(ctx) #Logs that the command has been triggered
+        help_menu = discord.Embed(title=bot_config["Name"], url=bot_config["Git_Repo"], description=bot_config["Description"], colour=colour_list["stings_light_gold"]) #Creates the help menu object and loads the data from the config file and the colour from the list
+        help_menu.set_thumbnail(url="https://ssago.org/img/clubs/logos/23.png") #Adds the club logo from the ssago website to the help menu
         for command in bot.commands:
-            help_menu.add_field(name=command.name, value=command.help, inline=False)
-        help_menu.set_footer(text="Any Problems, please contact Oscar Lodge (@ShadowedLord05) or the StinGS Webmaster (webmaster@stings.ssago.org)")
+            help_menu.add_field(name=command.name, value=command.help, inline=False) #Adds each command to the help menu with a description of what it does
+        help_menu.set_footer(text="Any Problems, please contact Oscar Lodge (@ShadowedLord05) or the StinGS Webmaster (webmaster@stings.ssago.org)") #Adds a message to the bottom of the menu
         await ctx.send(embed=help_menu)
--- colours.py
+++ colours.py
@@ -1,22 +1,21 @@
 from .imports import discord
 
-#Path where the colour codes are located
-path = r"Bot_Core\Brand\colours.scss"
+path = r"Bot_Core\Brand\colours.scss" #Path where the colour codes are located
 
-colours_file = open(path, "r")
+colours_file = open(path, "r") #Loads the colout file in read only mode
 
 colour_list = {}
 
 #Loads the colours from the general branding file and sets them to Discord readable values
 for line in colours_file:
-    if line[0] == "$":
-        colour_start = line.find("(") + 1
-        colour_end = line.find(")")
-        full_colour_data = line[colour_start:colour_end]
-        colour_name_end = line.find(":")
-        colour_data = full_colour_data.split(",")
-        colour_name = line[1:colour_name_end]
-        colour = discord.Colour.from_rgb(int(colour_data[0]), int(colour_data[1]), int(colour_data[2]))
-        colour_list.update({colour_name : colour})
+    if line[0] == "$": #Checks that the line is a colour value
+        colour_start = line.find("(") + 1 #Finds the position where the colour information starts
+        colour_end = line.find(")") #Finds the position where the colour value ends
+        full_colour_data = line[colour_start:colour_end] #Gets the RGBA values from the file as a single string
+        colour_data = full_colour_data.split(",") #Splits the RGBA values into individual values
+        colour_name_end = line.find(":") #Finds where the colour name ends
+        colour_name = line[1:colour_name_end] #Gets the substring of the colour name
+        colour = discord.Colour.from_rgb(int(colour_data[0]), int(colour_data[1]), int(colour_data[2])) #Creates the colour as a discord object from the data, ignores the alpha value as it is not supported
+        colour_list.update({colour_name : colour}) #Adds the Colour object to the dictionary with the name as the key
 
-colours_file.close()
+colours_file.close() #Closes the file
